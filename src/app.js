b16842c6eb2aa1fb34929793ebe97176eb2b964f
damiloyola <damiloyola@gmail.com>
2020-04-05 12:43:50 -0300

    config redux store

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
-import configStore from '../src/store/configureStore';
+import configStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configStore();
 
-console.log(store.getState());
+
+
+store.dispatch(addExpense( {description: 'agua' }));
+store.dispatch(addExpense( {description: 'gas' }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
